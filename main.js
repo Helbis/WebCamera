8d4b8f94499c8930658495b0955fe42224cdef32
Helbis <filippiotrhelbig@gmail.com>
2020-12-30 16:08:16 +0100

    I decided to work on that branch

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 let capture;
 
 function setup() {
-  createCanvas(480, 480);
-  capture = createCapture(VIDEO);
-  capture.hide();
+    createCanvas(480, 120);
+
+    let constraints = {
+        video: {
+            mandatory: {
+                minWidth: 1280,
+                minHeight: 720
+            },
+            optional: [{
+                maxFrameRate: 10
+            }]
+        },
+        audio: true
+    };
+
+    createCapture(constraints, stream => {
+        console.log(stream);
+    });
 }
 
 function draw() {
-  image(capture, 0, 0, width, width * capture.height / capture.width);
-  // filter(INVERT);
+    background(0);
+    image(capture, 0, 0, width, width * capture.height / capture.width);
 }
