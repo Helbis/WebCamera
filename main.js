ae21fe52d8f6861e1bf3ff1dc8ed896730682ca2
Helbis <filippiotrhelbig@gmail.com>
2020-12-30 16:46:12 +0100

    Testing capturing methods

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 let capture;
 
+// function setup() {
+//     createCanvas(800, 400);
+//
+//     let constraints = {
+//         video: {
+//             mandatory: {
+//                 minWidth: 1280,
+//                 minHeight: 720
+//             },
+//             optional: [{
+//                 maxFrameRate: 10
+//             }]
+//         },
+//         audio: true
+//     };
+//
+//     createCapture(constraints, stream => {
+//         console.log(stream);
+//     });
+// }
+//
+// function draw() {
+//     background(0);
+//     image(capture, 0, 0, width, width * capture.height / capture.width);
+// }
+
 function setup() {
-    createCanvas(800, 400);
-
-    let constraints = {
-        video: {
-            mandatory: {
-                minWidth: 1280,
-                minHeight: 720
-            },
-            optional: [{
-                maxFrameRate: 10
-            }]
-        },
-        audio: true
-    };
-
-    createCapture(constraints, stream => {
-        console.log(stream);
-    });
+  createCanvas(480, 480);
+  capture = createCapture(VIDEO);
+  capture.hide();
 }
 
 function draw() {
-    background(0);
-    image(capture, 0, 0, width, width * capture.height / capture.width);
+  image(capture, 0, 0, width, width * capture.height / capture.width);
+  filter(INVERT);
 }
