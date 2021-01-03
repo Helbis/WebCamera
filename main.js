7eee6e0cdbf9092183e070d48b0261c253c3ad87
Helbis <filippiotrhelbig@gmail.com>
2021-01-03 12:54:45 +0100

    Trying to get more general canvas element

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let capture;
+let canv;
+let canv2
 
 // let constraints = {
 //     video: {
@@ -16,7 +18,11 @@ let capture;
 
 
 function setup() {
-    createCanvas(480, 480);
+    canv = createCanvas(480, 480);
+    canv.id("MyCanvas");
+    // canv2 = createCanvas(480, 480);
+    // canv2.id("CANVAS_2");
+
     capture = createCapture(VIDEO);
     capture.size(480, 480);
     // capture.hide();
