1bffd331d846673b9d3a1ed8b297e0836946f453
Helbis <filippiotrhelbig@gmail.com>
2021-01-03 19:24:56 +0100

    Trying to get just the back camera to work

--- main.js
+++ main.js
@@ -8,29 +8,12 @@
     }
 
     // get page elements
-    const vidFront = document.querySelector("#videoInputFront");
     const vidBack = document.querySelector("#videoInputBack");
-    const btnCameraFront = document.querySelector("#getCameraFront");
     const btnCameraBack = document.querySelector("#getCameraBack");
     const btnGrabFrame = document.querySelector("#grabFrameButton");
     const canvas = document.querySelector("#grabFrameCanvas");
 
     // video constraints
-    const constraintsFront = {
-        video: {
-            width: {
-                min: 1280,
-                ideal: 1920,
-                max: 2560,
-            },
-            height: {
-                min: 720,
-                ideal: 1080,
-                max: 1440,
-            },
-            facingMode: "user"
-        },
-    };
     const constraintsBack = {
         video: {
             width: {
@@ -47,9 +30,7 @@
         },
     };
 
-
     // current video stream
-    let videoStreamFront;
     let videoStreamBack;
 
     // handle events
@@ -79,33 +60,16 @@
 
     // stop video stream
     function stopVideoStream() {
-        if (videoStream) {
-            videoStream.getTracks().forEach(track => {
+        if (videoStreamBack) {
+            videoStreamBack.getTracks().forEach(track => {
                 track.stop();
             });
         }
     }
 
-    // async function getDevices() {
-    //     const devices = await navigator.mediaDevices.enumerateDevices();
-    //     const para = document.createElement("p");
-    //     const node = document.createTextNode(devices);
-    //     para.appendChild(node);
-    //     const element = document.getElementById("div1");
-	//     element.appendChild(para);
-    // }
-
     // initialize
     async function initializeCamera() {
-        // stopVideoStream();
-        // constraints.video.facingMode = useFrontCamera ? "user" : "environment";
-
         try {
-            alert("Front camera")
-            videoStreamFront = await navigator.mediaDevices.getUserMedia(constraintsFront);
-            vidFront.srcObject = videoStreamFront;
-
-            alert("Back camera")
             videoStreamBack = await navigator.mediaDevices.getUserMedia(constraintsBack);
             vidBack.srcObject = videoStreamBack;
         } catch (err) {
